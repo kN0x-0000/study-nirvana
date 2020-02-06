a3d319eae0c92fcd29621cef2ebae90c8686974c
Nikhil Gahlot <43516957+kN0x-0000@users.noreply.github.com>
2020-02-06 15:05:19 +0530

    Update apps.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+from django.utils.autoreload import autoreload_started
 
+
+def my_watchdog(sender, **kwargs):
+	sender.watch_file('/tmp/foo.bar')
+	
 
 class AccountsConfig(AppConfig):
 	name = 'accounts'
